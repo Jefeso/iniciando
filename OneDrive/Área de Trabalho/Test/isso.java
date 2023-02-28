1e7ca88ed078bcfc87d6b1032f218b9248ffbfb1
Jefferson <jeffersokotoski@gmail.com>
2023-02-28 01:37:12 -0300

    comandos

---
+++ OneDrive/Área de Trabalho/Test/isso.java
@@ -0,0 +1,9 @@
+public class isso {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        System.out.println("world");
+        System.out.println("vamos lá");
+        System.out.print("to cansado");
+        System.out.print(" é a segunda branch");
+    }
+}
